9a80080cfc385ec4c2a1d9d725d6afd5ed65d207
Khairil Yusof <khairil.yusof@gmail.com>
2012-03-10 08:49:22 +0000

    Initial egg package with  buildout for sinar.corruption tracker.

---
+++ setup.py
@@ -0,0 +1,37 @@
+fromm setuptools import setup, find_packages
+import sys,os
+
+version = '0.1'
+
+setup(name='sinar.corruptiontracker',
+      version=version,
+      description="Sinar Corruption Tracker Application"
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Khairil Yusof',
+      author_email='khairil.yusof@gmail.com',
+      url='https://github.com/Sinar/sinar.corruptiontracker',
+      license='GPL',
+      packages=find_packages('src',exclude=['ez_setup']),
+      namespace_packages=['sinar'],
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'plone.app.dexterity',
+          'collective.autopermission'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
+~         
